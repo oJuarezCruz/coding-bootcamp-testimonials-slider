4dd5fd02f230432fd33b57a589d2996e27ca2ed5
Juarez Sant'Ana Vieira da Cruz <juarezsantana0897@gmail.com>
2023-10-03 09:10:31 -0300

    Limpei o código e fiz melhorias.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import Attribution from "./Components/Attribution"
 import Testimony from "./Components/Testimony";
-import patternCurve from './images/pattern-curve.svg';
+import patternCurve from './pattern-curve.svg';
+import imageTanya from './Components/Testimony/image-tanya.jpg';
+import imageJohn from './Components/Testimony/image-john.jpg';
+import { useState } from 'react';
 
 function App() {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const testimonies = [
+    {
+      src: imageTanya,
+      alt: "Tanya",
+      name: "Tanya Sinclair",
+      quote: "“I've been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I'm now in the job of my dreams and so excited about the future.”",
+      role: "UX Engineer"
+    },
+    {
+      src: imageJohn,
+      alt: "John",
+      name: "John Tarkpor",
+      quote: "“If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”",
+      role: "Junior Front-end Developer"
+    }
+  ]
+
   return (
     <div className="w-full min-h-screen font-inter flex items-start lg:items-center justify-center">
-      <Testimony />
+      <Testimony
+        src={testimonies[currentIndex].src}
+        alt={testimonies[currentIndex].alt}
+        name={testimonies[currentIndex].name}
+        quote={testimonies[currentIndex].quote}
+        role={testimonies[currentIndex].role}
+        onClickPrev={() => setCurrentIndex(0)}
+        onClickNext={() => setCurrentIndex(1)}
+      />
       <img
         src={patternCurve}
         alt=""
--- src/Components/Attribution/index.tsx
+++ src/Components/Attribution/index.tsx
@@ -1,6 +1,6 @@
 export default function Attribution() {
     return (
-        <footer className="w-full p-3 absolute inset-x-0 bottom-0 text-center text-txtMedium">
+        <footer className="w-full p-0 md:p-3 absolute inset-x-0 bottom-0 text-center text-txtMedium">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="font-bold hover:underline text-txtBold"> Frontend Mentor</a>.
             Coded by <a href="https://github.com/oJuarezCruz" className="font-bold hover:underline text-txtBold">Juarez Cruz.</a>
         </footer>
